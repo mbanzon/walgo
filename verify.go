72d6212651fe2746a5680b745c8f97fb160348d2
Michael Banzon <michael@banzon.dk>
2015-08-22 09:06:54 +0200

    Implementing HTTP body verification.

--- verify.go
+++ verify.go
@@ -1,15 +1,37 @@
 package walgo
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"reflect"
 )
 
-func RequireBody(r *http.Request, v interface{}, next func()) {
+func RequireBody(w http.ResponseWriter, r *http.Request, v interface{}, next func()) {
 	if reflect.TypeOf(v).Kind() == reflect.Struct {
+		t := reflect.TypeOf(v)
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+
+			data, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "", http.StatusInternalServerError)
+				return
+			}
+
+			tmp := make(map[string]interface{})
+			err = json.Unmarshal(data, &tmp)
+			if err != nil {
+				http.Error(w, "", http.StatusInternalServerError)
+			}
+
+			// TODO - check if all v fields is present
+		}
+
 		next()
 	} else {
-		fmt.Println("oh no")
+		http.Error(w, "", http.StatusInternalServerError)
+		return
 	}
 }
