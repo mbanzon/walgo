0fd73a57dde0cc6854a74e472cb150336012bac5
Michael Banzon <michael@banzon.dk>
2015-08-23 09:15:04 +0200

    Removed test

--- verify_test.go
+++ verify_test.go
@@ -1,14 +1,1 @@
 package walgo
-
-import (
-	"testing"
-)
-
-func TestSomeStuff(t *testing.T) {
-	s := struct {
-		Foo string `walgo:"skip"`
-	}{}
-
-	ok, err := verifyData([]byte(`{"Foo":"bar"}`), &s)
-	t.Log(ok, err, s)
-}
