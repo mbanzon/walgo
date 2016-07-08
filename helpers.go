10b51e01f6b06e7a1866620fd4561a5d75c13880
Michael Banzon <michael@banzon.dk>
2016-07-08 16:29:50 +0200

    Added comments for #2 and cleaning some code.

--- helpers.go
+++ helpers.go
@@ -11,24 +11,29 @@ const (
 	JsonContentType   = "application/json" // Content type for JSON payloads
 )
 
-// Checks the provided error. If it is nil the provided v is encoded as
-// JSON and written to the given response writer. If it is not nil
-// the status code 500 (Internal server error) is instead sent.
+// CheckErrOutputJson checks the provided error. If it is nil the provided
+// v is encoded as JSON and written to the given response writer. If it is
+// not nil the status code 500 (Internal server error) is instead sent.
 func CheckErrOutputJson(err error, w http.ResponseWriter, v interface{}) {
-	CheckErr(w, err, http.StatusInternalServerError, func() {
+	CheckErr(w, err, func() {
 		w.Header().Add(ContentTypeHeader, JsonContentType)
 		if e := json.NewEncoder(w).Encode(v); e != nil {
-			http.Error(w, "Internal error.", http.StatusInternalServerError)
+			http.Error(w, "Internal server error.", http.StatusInternalServerError)
 		}
 	})
 }
 
-func CheckErr(w http.ResponseWriter, err error, code int, next func()) {
+// CheckErr checks the provided error and if it is not nil it sends the
+// status code 500 (Internal server error) to the given ResponseWriter.
+//
+// There is a special case where the error provided is the sql.ErrNoRows.
+// In that case the status code 404 is returned.
+func CheckErr(w http.ResponseWriter, err error, next func()) {
 	if err != nil {
 		if err == sql.ErrNoRows {
 			http.Error(w, "", http.StatusNotFound)
 		} else {
-			http.Error(w, "", code)
+			http.Error(w, "", http.StatusInternalServerError)
 		}
 	} else {
 		next()
