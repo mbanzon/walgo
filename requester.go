f1eeb2cccfdea8c12cc9073877a4e712c3471ae9
Michael Banzon <michael@banzon.dk>
2016-07-12 09:47:32 +0200

    Adds comments for #2

--- requester.go
+++ requester.go
@@ -25,17 +25,52 @@ func init() {
 }
 
 type Requester interface {
+	// Get performs a GET reuqest to the given URL with the given parameters.
 	Get(url string, p ParameterMap) (res Response, err error)
+
+	// Post performs a POST request to the given URL with the given
+	// parameters and no request body.
 	Post(url string, p ParameterMap) (r Response, err error)
+
+	// PostJson performs a POST request to the given URL with the given
+	// parameters and the supplied interface type encoded as JSON.
 	PostJson(url string, p ParameterMap, v interface{}) (r Response, err error)
+
+	// PostRaw performs a POST request to the given URL with the given
+	// parameters and the supplied bytes as the request body.
 	PostRaw(url string, p ParameterMap, data []byte) (r Response, err error)
+
+	// PostMultipart performs a POST request to the given URL with the given
+	// parameters and the supplied multipart payload encoded as the request
+	// body.
 	PostMultipart(url string, p ParameterMap, m *MultipartPayload) (r Response, err error)
+
+	// PostValues performs a POST request to the given URL with the given
+	// parameters and a body consisting of the supplied values urlencoded.
 	PostValues(url string, p ParameterMap, v url.Values) (r Response, err error)
+
+	// Put performs a PUT request to the given URL with the given parameters.
 	Put(url string, p ParameterMap) (r Response, err error)
+
+	// PutJson performs a PUT request to the given URL with the given
+	// parameters and the supplied interface type encoded as JSON.
 	PutJson(url string, p ParameterMap, v interface{}) (r Response, err error)
+
+	// PutRaw performs a PUT request to the given URL with the given
+	// parameters and the supplied bytes as the request body.
 	PutRaw(url string, p ParameterMap, data []byte) (r Response, err error)
+
+	// PutMultipart performs a PUT request to the given URL with the given
+	// parameters and the supplied multipart payload encoded as the request
+	// body.
 	PutMultipart(url string, p ParameterMap, m *MultipartPayload) (r Response, err error)
+
+	// PutValues performs a PUT request to the given URL with the given
+	// parameters and a body consisting of the supplied values urlencoded.
 	PutValues(url string, p ParameterMap, v url.Values) (r Response, err error)
+
+	// Delete peforms a DELETE request to the given URL with the given
+	// parameters.
 	Delete(url string, p ParameterMap) (r Response, err error)
 
 	makeRequest(url string, p ParameterMap, method string, l *payload) (r Response, err error)
@@ -47,6 +82,10 @@ type requesterImpl struct {
 	authToken string
 }
 
+// NewRequester creates a new Requester using the supplied client. Every
+// request has the User-Agent header set to the given value and if the
+// authentication token differs from "" it is also used as the Authorization
+// header value (with the "Bearer "-prefix).
 func NewRequester(c *http.Client, userAgent, authToken string) (r Requester) {
 	return &requesterImpl{
 		client:    c,
@@ -110,4 +149,22 @@ func (f *requesterImpl) makeRequest(url string, p ParameterMap, method string, l
 	}
 
 	return r, err
+}
+
+func createParameterUrl(urlStr string, p ParameterMap) (u *url.URL, err error) {
+	u, err = url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	query := u.Query()
+
+	if p != nil {
+		for k, v := range p {
+			query.Add(k, v)
+		}
+	}
+
+	u.RawQuery = query.Encode()
+	return u, nil
 }
